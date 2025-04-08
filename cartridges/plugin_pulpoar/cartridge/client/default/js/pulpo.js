ba5aa03239935186534c868e77945b28e37dd7a8
Mehmet Akif Uygun <akif@aes.services>
2025-04-08 10:39:10 +0300

    Update pulpo.js
    Update condition

--- cartridges/plugin_pulpoar/cartridge/client/default/js/pulpo.js
+++ cartridges/plugin_pulpoar/cartridge/client/default/js/pulpo.js
@@ -1,47 +1,48 @@
 'use strict';
 
 function events() {
-    if (pulpoar) {
-        $(document).ready(function () {
-            pulpoar.onGoToProduct((payload) => {
-                if (payload[0].web_link) {
-                    window.location.replace(payload[0].web_link);
-                }
-            });
-            pulpoar.onAddToCart((payload) => {
-                var addToCartUrl = $('.pulpo-sdk').data('add-to-cart-url');
-                if (payload.length > 0) {
-                    payload.forEach(function(item) {
-                        if (addToCartUrl && item.slug) {
-                            var form = {
-                                pid: item.slug.toUpperCase(),
-                                quantity: 1
-                            };
-                            $.ajax({
-                                url: addToCartUrl,
-                                method: 'POST',
-                                data: form,
-                                success: function (response) {
-                                    $('.minicart').trigger('count:update', response);
-                                    var messageType = response.error ? 'alert-danger' : 'alert-success';
-                                    if ($('.add-to-cart-messages').length === 0) {
-                                        $('body').append('<div class="add-to-cart-messages"></div>');
-                                    }
-                                    $('.add-to-cart-messages').append(
-                                        '<div class="alert ' + messageType + ' add-to-basket-alert text-center" role="alert">'
-                                        + response.message + '</div>'
-                                    );
-                                    setTimeout(function () {
-                                        $('.add-to-basket-alert').remove();
-                                    }, 5000);
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        });
+    if (!pulpoar) {
+        return;
     }
+    $(document).ready(function () {
+        pulpoar.onGoToProduct((payload) => {
+            if (payload[0].web_link) {
+                window.location.replace(payload[0].web_link);
+            }
+        });
+        pulpoar.onAddToCart((payload) => {
+            var addToCartUrl = $('.pulpo-sdk').data('add-to-cart-url');
+            if (payload.length > 0) {
+                payload.forEach(function(item) {
+                    if (addToCartUrl && item.slug) {
+                        var form = {
+                            pid: item.slug.toUpperCase(),
+                            quantity: 1
+                        };
+                        $.ajax({
+                            url: addToCartUrl,
+                            method: 'POST',
+                            data: form,
+                            success: function (response) {
+                                $('.minicart').trigger('count:update', response);
+                                var messageType = response.error ? 'alert-danger' : 'alert-success';
+                                if ($('.add-to-cart-messages').length === 0) {
+                                    $('body').append('<div class="add-to-cart-messages"></div>');
+                                }
+                                $('.add-to-cart-messages').append(
+                                    '<div class="alert ' + messageType + ' add-to-basket-alert text-center" role="alert">'
+                                    + response.message + '</div>'
+                                );
+                                setTimeout(function () {
+                                    $('.add-to-basket-alert').remove();
+                                }, 5000);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
 }
 
 module.exports = {
